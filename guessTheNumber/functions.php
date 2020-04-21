5dd06c099f66ce718d508ecc18ce3bd14a04362c
root <tungnguyenls2008@gmail.com>
2020-04-21 15:56:25 +0700

    homework

--- guessTheNumber/functions.php
+++ guessTheNumber/functions.php
@@ -1,10 +1,38 @@
 <?php
-function createNumberArray(){
+function createNumberArray()
+{
     $nums = [];
     for ($i = 0; $i < 100; ++$i) {
         $nums[$i] = $i;
     }
-    for($i=0;$i<count($nums);$i++){
-        echo $nums[$i].', ';
+    return $nums;
+}
+
+function displayNumberArray($array)
+{
+    if (isset($_POST['submit'])) {
+        $num = $_POST['number'];
+        for ($i = 0; $i < count($array); $i++) {
+
+            if ($num == $array[$i]) {
+                $array[$i] = '<font size="7">' . $array[$i] . '</font>';
+            }
+            echo "$array[$i] , ";
+        }
+    }return NULL;
+}
+
+function binarySearch(array $arr, $start, $end, $x)
+{
+    if ($end < $start) {
+        return false;
+    }
+    $mid = floor(($end + $start) / 2);
+    if ($arr[$mid] == $x) {
+        return true;
+    } elseif ($arr[$mid] > $x) {
+        return binarySearch($arr, $start, $mid - 1, $x);
+    } else {
+        return binarySearch($arr, $mid + 1, $end, $x);
     }
 }
--- guessTheNumber/index.php
+++ guessTheNumber/index.php
@@ -1,3 +1,30 @@
 <?php
 include "functions.php";
-echo createNumberArray();
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body><form method="post">
+<input type="number" placeholder="your number" name="number">
+    <input type="submit" name="submit">
+    <?php
+    if (isset($_POST['submit'])){
+        $arr=createNumberArray();
+        $num=$_POST['number'];
+        $result=binarySearch($arr,0,100,$num);
+        if ($result){
+            echo displayNumberArray($arr);
+        }
+        //echo '<span style="font-size: xx-large; ">' .$result. '</span>';
+    }
+    ?>
+</form>
+</body>
+</html>
